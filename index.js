065769da43870953c21d3649dcce11d68494566b
sergey-lypko <lypko.sg@gmail.com>
2018-05-10 02:29:56 +0300

    Update

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log((4 in [1, 2, 3, 4]))
+// Дополнительные способы проверки и
+// получения значений полей объекта
+var myOb = {};
+
+Object.defineProperty(myOb, 'a', {
+  value: 1,
+  enumerable: true
+});
+
+Object.defineProperty(myOb, 'b', {
+  value: 2,
+  enumerable: false
+});
+
+myOb.propertyIsEnumerable('a'); // true
+myOb.propertyIsEnumerable('b'); // false
+
+
+// Object.keys и Object.getOwnPropertyNames -
+// оба идут только по базовым объектам, без
+// дальнейшего движения вверх по цепочке прототипов
+
+// Object.keys возвращает все enumerable
+// свойства объекта
+Object.keys(myOb); // ['a']
+
+// Object.getOwnPropertyNames возвращает все свойства,
+// enumerable или нет
+Object.getOwnPropertyNames(myOb); // ['a', 'b']
