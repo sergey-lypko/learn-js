8dd6e2b02463663d7726c438213d3dc53c9df8e2
Sergey Lypko <lypko.sg@gmail.com>
2018-09-13 00:41:41 +0300

    Update classes.js

--- this_and_prototypes/prototypes/classes.js
+++ this_and_prototypes/prototypes/classes.js
@@ -1,1 +1,16 @@
+// Важно помнить, что классы - это все та же прототипаная модель только
+// с чуть более приятным синтаксисом
+class C {
+  constructor() {
+    C.prototype.counter++;
+  }
+}
 
+C.prototype.counter = 0;
+
+const c1 = new C();
+const c2 = new C();
+
+// И проблемы возникают те же (например в данном случае - перекрытие):
+console.log(c2.counter); // 2
+console.log(c1.counter === c2.counter); // true
