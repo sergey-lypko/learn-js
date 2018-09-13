ae4e62bc46b05f7b73072c68486c7573e881d68c
Sergey Lypko <lypko.sg@gmail.com>
2018-09-14 00:46:39 +0300

    Update OLOO_style.js

--- this_and_prototypes/prototypes/OLOO_style.js
+++ this_and_prototypes/prototypes/OLOO_style.js
@@ -177,3 +177,26 @@ let mouse = Object.assign(Object.create(animal), {
   legs: 4,
   tail: 'long, skinny'
 });
+
+
+// * * *
+
+let animal = {
+  animalType: 'animal',
+ 
+  describe () {
+    return `An ${this.animalType} with ${this.furColor} fur, 
+      ${this.legs} legs, and a ${this.tail} tail.`;
+  }
+};
+ 
+let mouseFactory = function mouseFactory () {
+  return Object.assign(Object.create(animal), {
+    animalType: 'mouse',
+    furColor: 'brown',
+    legs: 4,
+    tail: 'long, skinny'
+  });
+};
+
+let mickey = mouseFactory();
