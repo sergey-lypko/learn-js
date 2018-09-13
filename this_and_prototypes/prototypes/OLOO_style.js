b80d48c51b7dc81e458bd2a52bbac54c5729f12f
Sergey Lypko <lypko.sg@gmail.com>
2018-09-14 00:36:18 +0300

    Update OLOO_style.js

--- this_and_prototypes/prototypes/OLOO_style.js
+++ this_and_prototypes/prototypes/OLOO_style.js
@@ -159,3 +159,21 @@ const modelX = Object.create(Car);
 modelX.setup('electro', 772, 'Model S');
 modelX.engineTurnOn();
 modelX.showSpec(); // electro 772 Model S . Engine is working:  true
+
+
+// Примеры использования OLOO
+let animal = {
+  animalType: 'animal',
+  
+  describe () {
+    return `An ${this.animalType}, with ${this.furColor} fur, 
+      ${this.legs} legs, and a ${this.tail} tail.`;
+  }
+};
+
+let mouse = Object.assign(Object.create(animal), {
+  animalType: 'mouse',
+  furColor: 'brown',
+  legs: 4,
+  tail: 'long, skinny'
+});
