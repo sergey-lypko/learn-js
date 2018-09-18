d393c7b64093a2a60b53b79685d80ce10f55ff15
Sergey Lypko <lypko.sg@gmail.com>
2018-09-18 18:20:49 +0300

    Update 2_parallel_threading.js

--- asycn_and_performance/asynchrony_now_and_later/2_parallel_threading.js
+++ asycn_and_performance/asynchrony_now_and_later/2_parallel_threading.js
@@ -29,5 +29,19 @@ ajax( "http://some.url.2", bar ); // *2
 
 
 // Выполнение когда на JavaScript происходит согласно правилу Run-to-Completion, 
-// которое означает, что если стандатрная функция начинает выполнение, то весь ее код
+// которое означает, что если стандатрная (не генератор) функция начинает выполнение, то весь ее код
 // будет выполнен до того, как начнется выполнение когда в какой-либо другой функции.
+
+// Из MDN:
+// Each message is processed completely before any other message is processed.
+// This offers some nice properties when reasoning about your program,
+// including the fact that whenever a function runs, it cannot be pre-empted and
+// will run entirely before any other code runs (and can modify data the function manipulates).
+// This differs from C, for instance, where if a function runs in a thread,
+// it may be stopped at any point by the runtime system to run some other code in another thread.
+
+// A downside of this model is that if a message takes too long to complete,
+// the web application is unable to process user interactions like click or scroll.
+// The browser mitigates this with the "a script is taking too long to run" dialog.
+// A good practice to follow is to make message processing short and
+// if possible cut down one message into several messages.
